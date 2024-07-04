7177ae38f8cfd69cfa42047a43c1d1ace4369686
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-07-04 17:16:16 -0300

    feat: dados históricos raspado com sucesso

--- src/automation/data_scraping_stations.py
+++ src/automation/data_scraping_stations.py
@@ -1,47 +1,45 @@
-# import requests
-# from bs4 import BeautifulSoup
 import time
+import pandas as pd
 from ..utils.selenium_automation_tools import AutomationTools
 from selenium.webdriver.common.by import By
 
 auto_tools = AutomationTools()
 
 class DataScrapingStations:
-
+            
     @staticmethod
     def start():
         auto_tools.init_chrome_driver()
         auto_tools.go_to_page()
         data_stations = DataScrapingStations.get_tr_xpaths()
 
-        all_data = []
+        general_data = []
         driver = auto_tools.get_driver()
 
         for station in data_stations:
-            # print(xpath)
-            auto_tools.click_xpath(xpath_btn=station['xpath'])
+            # print(xpath) 
+            # auto_tools.click_xpath(xpath_btn=station['xpath'])
+            auto_tools.click_xpath(xpath_btn='/html/body/table/tbody/tr[17]/td[1]/a')
 
             xpath_latitude = driver.find_element(By.XPATH, '/html/body/center/b/table/tbody/tr[2]/td[5]').text
             xpath_longitude = driver.find_element(By.XPATH, '/html/body/center/b/table/tbody/tr[2]/td[6]').text
 
-            # tabela de dados históricos
-            # /html/body/center/b/center/center/table/tbody/tr[1]/td[1]
-            columns = DataScrapingStations.get_value_td_table()
-            print('colunas: ', columns)
+            df_historical_data = DataScrapingStations.get_values_td_table()
 
-            all_data.append({
-                'ID externo da estação': station['id_station'],
-                'Nome da estação': station['name_station'],
+            general_data.append({
+                'ID externo da estacao': station['id_station'],
+                'Nome da estacao': station['name_station'],
                 'Latitude': xpath_latitude,
                 'Longitude': xpath_longitude,
             })
 
             # auto_tools.go_to_page()
             time.sleep(2)
-            print(all_data)
             break
-        # print(xpaths)
-        return data_stations
+
+        df_all_data = pd.DataFrame(general_data)
+
+        return {'all_data': df_all_data, 'historical_data': df_historical_data}
     
     @staticmethod
     def get_tr_xpaths():
@@ -65,7 +63,7 @@ class DataScrapingStations:
         return data_stations
     
     @staticmethod
-    def get_value_td_table():
+    def get_value_columns_table():
         driver = auto_tools.get_driver()
         tr_elements = driver.find_elements(By.XPATH, "/html/body/center/b/center/center/table/tbody/tr[1]/td")
         title_columns_stations = []
@@ -78,18 +76,28 @@ class DataScrapingStations:
             })
         return title_columns_stations
     
+    @staticmethod
+    def get_values_td_table():
+        driver = auto_tools.get_driver()
+        # Primeiro, extraia o cabeçalho da tabela
+        header_elements = driver.find_elements(By.XPATH, "/html/body/center/b/center/center/table/tbody/tr[1]/td")
+        headers = [header.text for header in header_elements]
+
+        # Agora, extraia os dados das linhas da tabela
+        tr_elements = driver.find_elements(By.XPATH, "/html/body/center/b/center/center/table/tbody/tr[position()>1]")
+
+        data_rows = []
+
+        for tr_element in tr_elements:
+            td_elements = tr_element.find_elements(By.TAG_NAME, "td")
+            row_data = [td_element.text for td_element in td_elements]
+            data_rows.append(row_data)
+
+        df = pd.DataFrame(data_rows, columns=headers)
+
+        return df
+    
+
     
     
 
-# /html/body/center/b/center/center/table/tbody/tr[2]
-
-# /html/body/table/tbody/tr[{index}]/td[1]/a
-# /html/body/table/tbody/tr[{index}]/td[2]/a
-
-
-
-    # def fetch_weather_data(url: str):
-    #     auto_tools.init_chrome_driver()
-    #     auto_tools.go_to_page()
-    #     auto_tools.click_xpath(xpath_btn='/html/body/table/tbody/tr[3]/td[1]/a')
-    #     time.sleep(5)
