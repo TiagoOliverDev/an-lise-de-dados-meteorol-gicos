e06082409e1ccb0407883cbd011e146ea4927fef
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-07-05 09:19:52 -0300

    feat: validação no repo

--- src/repositories/historic_data_repository.py
+++ src/repositories/historic_data_repository.py
@@ -5,7 +5,18 @@ class HistoricDataRepository:
     def __init__(self, db: Session):
         self.db = db
 
-    def create_historic_data(self, name_data: str, value_data: float, station_id: int):
+    def create_historic_data(self, name_data: str, value_data: str, station_id: int) -> HistoricData:
+        if name_data == 'DataHora (GMT)':
+            existing_data = self.db.query(HistoricData).filter(
+                HistoricData.name_data == name_data,
+                HistoricData.value_data == value_data,
+                HistoricData.station_id == station_id
+            ).first()
+
+            if existing_data:
+                print(f"Dados históricos já existem no banco de dados para station_id '{station_id}' com DataHora '{value_data}'.")
+                return existing_data
+
         historic_data = HistoricData(name_data=name_data, value_data=value_data, station_id=station_id)
         self.db.add(historic_data)
         self.db.commit()
@@ -14,5 +25,4 @@ class HistoricDataRepository:
 
     def get_historic_data_by_id(self, data_id: int):
         return self.db.query(HistoricData).filter(HistoricData.id == data_id).first()
-    
-    # Adicione outros métodos conforme necessário
+
--- src/repositories/station_repository.py
+++ src/repositories/station_repository.py
@@ -5,14 +5,24 @@ class StationRepository:
     def __init__(self, db: Session):
         self.db = db
 
-    def create_station(self, name: str, latitude: float, longitude: float):
-        station = Station(name=name, latitude=latitude, longitude=longitude)
-        self.db.add(station)
-        self.db.commit()
-        self.db.refresh(station)
-        return station
+    def create_station(self, name: str, latitude: str, longitude: str):
+            existing_station = self.db.query(Station).filter(
+                Station.name == name,
+                Station.latitude == latitude,
+                Station.longitude == longitude
+            ).first()
+            
+            if existing_station:
+                print(f"Estação '{name}' já existe no banco de dados.")
+                return existing_station
+
+            station = Station(name=name, latitude=latitude, longitude=longitude)
+            self.db.add(station)
+            self.db.commit()
+            self.db.refresh(station)
+            return station
 
     def get_station_by_id(self, station_id: int):
         return self.db.query(Station).filter(Station.id == station_id).first()
     
-    # Adicione outros métodos conforme necessário
+
